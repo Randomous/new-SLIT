50675632317919ebf27aef0ada58b29774c266d7
Daniel Pettersen <dtpett16@student.uia.no>
2017-11-12 14:51:19 +0100

    Removed unused source files (#69)
    Nearly all old files that used Hash for the program are gone. This includes the following packages: ContentHandler, InformationInterface, Module, ModuleRegister, Register, student and StudentRegister, as well as InputAndResponses.CommandFolder, and InputAndResponses. WebInput is also deleted. Only Input.java in WebInput has been modified, as well as Start.java, to allow the program to start and display the correct headings in the home page. Files changed in WEB-INF and build are arbitrary, and get changed any time someone makes a change.

--- UiA-SLIT/src/java/WebInput/Input.java
+++ UiA-SLIT/src/java/WebInput/Input.java
@@ -1,69 +1,22 @@
 package WebInput;
 import javax.jws.WebService;
 import InputAndResponses.Start;
-import java.util.HashSet;
-import InputAndResponses.CommandFolder.Commands;
-import Content.ModuleRegister;
 
 
     @WebService
 public class Input {
-        
-        public String name;
-        public String help;
+        public String welcome;
         public Start start;
-        public Commands com;
-        public String commands;
-        public ModuleRegister module;
         
 public Input() {
-        name = null;
-        help = null;
-       commands = null;
-       start = new Start();
-       com = new Commands();
-       module = new ModuleRegister();
+        welcome = null;
+        start = new Start();
     }
     
-    //Get and return the module as string
-    public String getModule() {
-        return module.getRegister();
-    }
-    //get name returned as string
-    public String getName() {
-       return name;
-    }
     //get help from method, returns string
     public String getHelp() {
         
-        return help = start.GetWebWelcome();
-    }
-    //get commands returned as string
-    public String getCommands() {
-        return commands = com.displayCommands();
-    }
-    //set help
-    public void setHelp(String help) {
-         
-         help = start.GetWebWelcome();
-         //help = "tettet";
-         //this.help = help;
-    }
-    //Set name from input
-    public void setName(String name) {
-       //this.name = name;
-        //Start start = new Start();
-        HashSet<String> find = new HashSet<>();
-        String newname = name.trim().toUpperCase();
-        String[] wordArray = newname.split(" ");
-        
-        for(String word : wordArray) {
-        find.add(word);
-        }
-        
-        //find.add(name);
-        name = start.start(find);
-        this.name = name;
+        return welcome = start.GetWebWelcome();
     }
 }
     
